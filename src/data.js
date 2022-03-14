f37053af9cc34b2a5b8e412b24d6450fd760deaa
Nicholas G <nick2123@gmail.com>
2022-03-13 19:48:20 -0700

    almost done

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/PHOSHPr.png",
+    link: "https://ngolston.github.io/FOMO_Cal/",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/QTt6yQG.png",
+    link: "https://infinite-ravine-51041.herokuapp.com",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/9l0JKnz.png",
+    link: "https://ngolston.github.io/Password-Generator/",
+  },
+  {
+    id: 4,
+    img: "https://i.imgur.com/ONLJquk.jpg",
+    link: "https://ngolston.github.io/Code-Quiz/",
+  },
+  {
+    id: 5,
+    img: "https://i.imgur.com/KPRiCEU.png",
+    link: "https://ngolston.github.io/day-planner/",
+  },
+  {
+    id: 6,
+    img: "https://i.imgur.com/oAsiixA.png",
+    link: "https://note-taking999.herokuapp.com",
+  },
+];
